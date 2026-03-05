7ec98100051c6940916cd841b9d1274dfb5e6f90
Matias Paredes <mmatiasparedes@gmail.com>
2026-03-04 23:42:06 -0300

    Corregido el codigo Arduino a Heltec

--- ArduinoToHeltecSerial/ArduinoToHeltecSerial.ino
+++ ArduinoToHeltecSerial/ArduinoToHeltecSerial.ino
@@ -1,25 +1,77 @@
-#include <SoftwareSerial.h>
+#include <SPI.h> //Serial Peripheral Interface
+#include <Ethernet.h> //Para conectarnos con el Ethernet Shield
+#include <ArduinoModbus.h> //Para utilizar modbus
+#include <SoftwareSerial.h> //para utilizar un puerto serie en el pin que se me cante
 
-#define RX_PIN 48
-#define TX_PIN 47 
-#define LED_PIN 35
+// Network configuration
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 0, 17);        // Arduino IP
+IPAddress server(192, 168, 0, 201);    // Modbus slave simulator IP
 
-SoftwareSerial SerialFromArduino(RX_PIN, TX_PIN);
+SoftwareSerial SerialToESP(31, 30); // le pongo los pines que quiero
+
+EthernetClient ethClient;
+ModbusTCPClient modbusTCPClient(ethClient); //comando principal para enviar comandos Modbus
 
 void setup() {
-  Serial.begin(9600);               
-  SerialFromArduino.begin(9600);    
-  pinMode(LED_PIN, OUTPUT);
-  Serial.println("Esperando datos...");
-}
+  Serial.begin(9600);
+  SerialToESP.begin(9600); //inicializo el serial virtual
+  while (!Serial);
+  
+  // Initialize Ethernet
+  Ethernet.begin(mac, ip);
 
-void loop() {
-  if (SerialFromArduino.available()) {
-    String mensaje = SerialFromArduino.readStringUntil('\n');
-    Serial.print("Recibimos: ");
-    Serial.println(mensaje);
-
-    digitalWrite(LED_PIN, !digitalRead(LED_PIN)); 
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) { //Ver que esté físicamente conectado 
+    Serial.println("W5100 not found!");
   }
-
+  
+  if (Ethernet.linkStatus() == LinkOFF) { //ver que haya enlace con el cable de red
+    Serial.println("Ethernet cable disconnected");
+  }
+  
+  Serial.println("Modbus TCP Client ready");
 }
+
+void loop() { //aca vemos si esta conectado, si no, intentamos conectar
+  if (!modbusTCPClient.connected()) {
+    Serial.println("Connecting to Modbus server...");
+    
+    if (!modbusTCPClient.begin(server, 502)) {
+      Serial.println("Connection failed!");
+      delay(5000);
+      return;
+    }
+    Serial.println("Connected to Modbus server!");
+  }
+  
+  delay(1000);
+
+  byte empaquetado = 0; //creo un byte, y voy leyendo y guardando el estado de cada coil
+
+    for (int i = 0; i < 8; i++) {
+      int estado = modbusTCPClient.coilRead(i);
+
+      if (estado == -1) {
+        Serial.print("Error leyendo coil ");
+        Serial.println(i);
+      } else {
+       bitWrite(empaquetado, i, estado);
+        Serial.print("Coil ");
+        Serial.print(i);
+        Serial.print(": ");
+        Serial.println(estado);
+      }
+    }
+
+    Serial.println("-----------------------");
+    Serial.print("Enviando : ");
+    Serial.println(empaquetado, BIN);
+    //Serial.println("Coils empaquetados (BIN): ");
+    //for (int b = 7; b >= 0; b--) {  // recorre los bits de mayor a menor
+    //  Serial.print(bitRead(empaquetado, b));
+    //  Serial.println();
+
+     SerialToESP.write(empaquetado); //mando crudo el dato
+    
+    delay(10000);
+  }
